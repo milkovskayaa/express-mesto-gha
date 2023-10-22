2f8d48e38d4839039cc87aeadc560a0226f43a80
Tatyana Maltseva <nucha1405@yandex.ru>
2023-10-22 11:57:25 +0300

    feat: подключить к базе данных

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const { PORT = 3000 } = process.env;
 const app = express();
+const mongoose = require('mongoose');
 
+// подключение к базе данных
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+}).then(() => {
+    console.log("Connected to MongoDB");
+});
 
 
 app.listen(PORT, () => {
