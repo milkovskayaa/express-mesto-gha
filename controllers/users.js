c12135007f43d6a5f0d1cacdf299c5d26212c945
Tatyana Maltseva <nucha1405@yandex.ru>
2023-10-22 20:01:38 +0300

    feat: создать контроллеры для пользователя

---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+const User = require("../models/user");
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+const getUserById = (req, res) => {
+  const { id } = req.params;
+
+  User.findById(id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "Пользователь не найден" });
+      }
+      return res.status(200).send(user);
+    })
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser
+};
