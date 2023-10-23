8a6b19d5042433be60b470a046ae59846dcb4b34
Tatyana Maltseva <nucha1405@yandex.ru>
2023-10-23 22:02:58 +0300

    feat: создать роуты для пользователя

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const { PORT = 3000 } = process.env;
-const app = express();
 const mongoose = require('mongoose');
-
+const userRouter = require('./routes/users');
 // подключение к базе данных
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
     useNewUrlParser: true,
 }).then(() => {
     console.log("Connected to MongoDB");
 });
 
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use('/users', userRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
