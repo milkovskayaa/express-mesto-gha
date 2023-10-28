6982b129caa55cd49b333d11dc53accc61bd2a44
Tatyana Maltseva <nucha1405@yandex.ru>
2023-10-28 18:31:22 +0300

    hotfixes: исправить запрос на создание юзера

--- controllers/users.js
+++ controllers/users.js
@@ -26,7 +26,7 @@ const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.status(200).send(user))
     .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };
 
