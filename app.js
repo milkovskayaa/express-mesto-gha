f97961b1f0d616faa5d6b669f83f0964de83a2bf
Tatyana Maltseva <nucha1405@yandex.ru>
2023-10-28 14:19:06 +0300

    feat: добавить id пользователя

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
+const { PORT = 3000 } = process.env;
+
 // подключение к базе данных
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
     useNewUrlParser: true,
@@ -8,10 +10,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
     console.log("Connected to MongoDB");
 });
 
-const { PORT = 3000 } = process.env;
-const app = express();
 
+const app = express();
+app.use(express.json());
 app.use('/users', userRouter);
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6536b716f4df280ddc201442'
+  };
+
+  next();
+});
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
