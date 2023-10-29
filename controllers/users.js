d44c852552db97b674d52fefe734efc57138b63a
Tatyana Maltseva <nucha1405@yandex.ru>
2023-10-29 19:41:06 +0300

    feat: добавить проверку введенных данных

--- controllers/users.js
+++ controllers/users.js
@@ -33,7 +33,7 @@ const createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(200).send(user))
     .catch(() =>
-      res.status(400).send({ message: "Данные введены некорректно" })
+      res.status(500).send({ message: "Произошла ошибка" })
     );
 };
 
--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,19 @@ const {
 
 userRouter.get("/", getUsers);
 userRouter.get("/:id", getUserById);
-userRouter.post("/", createUser);
+
+userRouter.post(
+  "/",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+      avatar: Joi.string().required().regex(urlPattern),
+    }),
+  }),
+  createUser
+);
+
 userRouter.patch(
   "/me",
   celebrate({
