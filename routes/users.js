78a4fd0c094537ad6bb9361f37608c531f188ac0
Tatyana Maltseva <nucha1405@yandex.ru>
2023-10-29 19:55:37 +0300

    hotfixes: исправить ошибки

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,4 @@
 const userRouter = require("express").Router();
-const { celebrate, Joi } = require("celebrate");
-Joi.objectId = require("joi-objectid")(Joi);
 const {
   getUsers,
   getUserById,
@@ -9,44 +7,10 @@ const {
   updateAvatar,
 } = require("../controllers/users");
 
-const urlPattern = new RegExp(
-  "^((http|https):\\/\\/)?(www\\.)?[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,6})+[a-zA-Z0-9-._~:\\/?#\\[\\]@!$&'()*+,;=]*$"
-);
-
 userRouter.get("/", getUsers);
 userRouter.get("/:id", getUserById);
-
-userRouter.post(
-  "/",
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
-      avatar: Joi.string().required().regex(urlPattern),
-    }),
-  }),
-  createUser
-);
-
-userRouter.patch(
-  "/me",
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
-    }),
-  }),
-  updateProfile
-);
-
-userRouter.patch(
-  "/me/avatar",
-  celebrate({
-    body: Joi.object().keys({
-      avatar: Joi.string().required().regex(urlPattern),
-    }),
-  }),
-  updateAvatar
-);
+userRouter.post("/", createUser);
+userRouter.patch("/me", updateProfile);
+userRouter.patch("/me/avatar", updateAvatar);
 
 module.exports = userRouter;
