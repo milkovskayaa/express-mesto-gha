060e2b816067a4a4673afedfc4f490f3939e02e6
Tatyana Maltseva <nucha1405@yandex.ru>
2023-11-02 19:53:05 +0300

    feat: добавить дизлайк карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,37 +1,37 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
 
 // получить все карточки
 const getCards = (req, res) => {
   Card.find({})
-  .then((cards) => res.status(200).send(cards))
-  .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
-}
+    .then((cards) => res.status(200).send(cards))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
 
 // создать карточку
 const createCard = (req, res) => {
   const { name, link, owner = req.user._id } = req.body;
   return Card.create({ name, link, owner })
-  .then((card) => res.status(200).send(card))
-  .catch((err) => {
-    if (err.name === "ValidationError") {
-      return res.status(400).send({ message: "Введены некорректные данные" });
-    }
-    return res.status(500).send({ message: "Произошла ошибка" });
-  });
-}
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Введены некорректные данные" });
+      }
+      return res.status(500).send({ message: "Произошла ошибка" });
+    });
+};
 
 // удалить карточку
 const deleteCard = (req, res) => {
   const { id } = req.params;
   Card.findByIdAndRemove(id)
-  .then((card) => {
-    if (!card) {
-      return res.status(404).send({ message: "Карточка не найдена" });
-    }
-    return res.status(200).send(card);
-  })
-  .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
-}
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: "Карточка не найдена" });
+      }
+      return res.status(200).send(card);
+    })
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
 
 // постановка лайка на карточку
 const addLikeCard = (req, res) => {
@@ -39,20 +39,38 @@ const addLikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     id,
     { $addToSet: { likes: req.user._id } },
-    { new: true },
+    { new: true }
   )
-  .then((card) => {
-    if (!card) {
-      return res.status(404).send({ message: "Карточка не найдена" });
-    }
-    return res.status(200).send(card);
-  })
-  .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
-}
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: "Карточка не найдена" });
+      }
+      return res.status(200).send(card);
+    })
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+// дизлайк карточки
+const removeLikeCard = (req, res) => {
+  const { id } = req.params;
+  Card.findByIdAndUpdate(
+    id,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true }
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: "Карточка не найдена" });
+      }
+      return res.status(200).send(card);
+    })
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
 
 module.exports = {
   getCards,
   createCard,
   deleteCard,
-  addLikeCard
-}
+  addLikeCard,
+  removeLikeCard,
+};
--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,16 @@
-const cardRouter = require('express').Router();
-const { getCards,createCard,deleteCard,addLikeCard } = require('../controllers/cards');
+const cardRouter = require("express").Router();
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  addLikeCard,
+  removeLikeCard,
+} = require("../controllers/cards");
 
-cardRouter.get('/', getCards);
-cardRouter.post('/', createCard);
-cardRouter.delete('/:id', deleteCard);
-cardRouter.put('/:id/likes', addLikeCard);
+cardRouter.get("/", getCards);
+cardRouter.post("/", createCard);
+cardRouter.delete("/:id", deleteCard);
+cardRouter.put("/:id/likes", addLikeCard);
+cardRouter.delete("/:id/likes", removeLikeCard);
 
 module.exports = cardRouter;
