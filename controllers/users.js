59cd5ffe422651dca7679fb0fd2a9d5478705a0c
Tatyana Maltseva <nucha1405@yandex.ru>
2023-12-11 17:00:25 +0300

    feat: изменить запрос createUser

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const {
   NOT_FOUND,
@@ -36,8 +37,15 @@ const getUserById = (req, res) => {
 
 // создать пользователя
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  // const { name, about, avatar, email, password } = req.body;
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      name: req.body.name,
+      about: req.body.about,
+      avatar: req.body.avatar,
+      email: req.body.email,
+      password: hash,
+    }))
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
