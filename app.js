092e70aae97445933e1c8876c09c0f0fa9720bbe
Tatyana Maltseva <nucha1405@yandex.ru>
2023-12-24 21:39:47 +0300

    feat: добавить валидацию

--- app.js
+++ app.js
@@ -19,7 +19,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 const app = express();
 app.use(express.json());
 
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
