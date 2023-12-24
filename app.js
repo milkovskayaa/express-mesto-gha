70b56fb158e5e10a7f8e05673ab71ddc68ed1760
Tatyana Maltseva <nucha1405@yandex.ru>
2023-12-24 21:33:02 +0300

    hotfixes: изменить валидацию

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
