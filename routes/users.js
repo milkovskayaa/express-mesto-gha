e0353e6798883353a4600a32d8ef88a4f0029cac
Tatyana Maltseva <nucha1405@yandex.ru>
2023-12-29 20:18:15 +0300

    feat: добавить валидацию Joi

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const userRouter = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers,
   getUserById,
@@ -9,9 +9,25 @@ const {
 } = require('../controllers/users');
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUserById);
 userRouter.get('/me', getUserInfo);
-userRouter.patch('/me', updateProfile);
-userRouter.patch('/me/avatar', updateAvatar);
+
+userRouter.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24).required(),
+  }),
+}), getUserById);
+
+userRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfile);
+
+userRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
+  }),
+}), updateAvatar);
 
 module.exports = userRouter;
