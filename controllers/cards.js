bc316a661fca0b3d90e2c75094d74b5a758d778d
Tatyana Maltseva <nucha1405@yandex.ru>
2023-12-30 20:16:40 +0300

    hotfixes: исправить код удаления карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -35,12 +35,11 @@ const deleteCard = (req, res, next) => {
       if (card.owner.toString() !== req.user._id) {
         throw new ForbiddenError('Вы не можете удалять чужую карточку');
       }
-      return Card.findByIdAndDelete(id);
     })
-    // .then((card) => {
-    //   Card.findByIdAndDelete(id);
-    //   return res.status(200).send(card);
-    // })
+    .then((card) => {
+      Card.findByIdAndRemove(id);
+      return res.status(200).send(card);
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
         next(new BadRequestError('Некорректный ID'));
